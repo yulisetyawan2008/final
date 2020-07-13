4f7348cadf11fad7d30b26b6a629a4bd86fd11c5
yuli setyawan <yulisetyawan2008@gmail.com>
2020-07-14 06:19:27 +0800

    pertama commit

--- resources/views/question/show.blade.php
+++ resources/views/question/show.blade.php
@@ -1,24 +1,27 @@
 @extends('layouts.master')
 
 @section('content')
-    <div>
-        <div>
-            <h1>Jawaban Pertanyaan</h1>
-            <h3>Judul : {{ $question->judul }}</h3>
-            <h5>Isi Pertanyaan : {!! $question->isi!!}</h5>
+    <div class="card text-white bg-success mb-3 w-75">
+        <div class="card-header">Jawaban Pertanyaan</div>
+        <div class="card-body">
+            <h3 class="card-tittle">Judul : {{ $question->judul }}</h3>
+            <p class="card-text">Isi Pertanyaan : {!! $question->isi!!}</p>
             @foreach($question->tags as $tag)
                 <button class="btn btn-primary btn-sm"> {{$tag->tag_name}}</button>
-
             @endforeach
-            <div>
+            <div class="card-body text-danger bg-white mt-3">
                 <h2>Jawaban</h2>
                 @foreach($answers as $answer)
                     <p>{!! $answer->isi!!}  Tanggal dibuat : {{ $answer->created_at}}, Tanggal Diperbaharui : {{ $answer->updated_at}}</p>
                 @endforeach
+
+                <a href="/question" class="btn btn-primary">Kembali</a>
             </div>
             
         </div>
-        <a href="/question" class="btn btn-sm btn-success">Kembali</a>
+        
     </div>
+
+ 
 
 @endsection
